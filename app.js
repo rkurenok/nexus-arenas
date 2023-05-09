5330e061ceb07d118452e5ab9c8980e33b3d115d
rkurenok <48415490+rkurenok@users.noreply.github.com>
2023-05-09 19:26:01 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -161,3 +161,21 @@ function battle(gladiator1, gladiator2) {
 function scrollPage() {
   window.scrollTo(0, document.body.scrollHeight);
 }
+
+// Проверка поддержки webp, добавление класса webp или no-webp для HTML
+function isWebp() {
+  // Проверка поддержки webp
+  function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+  }
+  // Добавление класса _webp или _no-webp для HTML
+  testWebP(function (support) {
+    var className = support === true ? 'webp' : 'no-webp';
+    document.documentElement.classList.add(className);
+  });
+}
+isWebp();
